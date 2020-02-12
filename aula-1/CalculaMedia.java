a795bd48ae4ea81e51c7714b35e2aec40743d28e
layzacs <layza.cristini@gmail.com>
2020-02-12 15:22:29 -0300

    cu

--- aula-1/CalculaMedia.java
+++ aula-1/CalculaMedia.java
@@ -1,5 +1,30 @@
 public class CalculaMedia {
     public static void main() {
 
+
+
+        Scanner teclado = new Scanner(System.in);
+
+        int aux = 1;
+        while (aux < 4) {
+            System.out.print("Projeto " + aux + ":");
+            int projeto = teclado.nextInt();
+            
+        }
+
+        int aux = 1;
+        while (aux < 8) {
+            System.out.print("Lista " + aux + ":");
+            int lista = teclado.nextInt();
+
+        }
+
+
+
+
+
+        System.out.println("O valor informado é: " + valor);
+
+
     }
 }
